bb18b6ca48c502f817b5befc010802bcef8ba5ec
ChernenkoRuslan <chernenko.ruslan@gmail.com>
2022-09-27 16:04:01 +0300

    25

---
+++ Day 25 - Running time and complexity.py
@@ -0,0 +1,15 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from math import sqrt
+from sys import stdin
+def checkPrime(n):
+    for i in range(2, int(sqrt(n))+1):
+        if n % i is 0:
+            return False
+    return True
+n = int(input())
+for line in stdin:
+    val = int(line)
+    if (val >= 2 and checkPrime(val)):
+        print("Prime")
+    else:
+        print("Not prime")
